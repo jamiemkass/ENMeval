43d3612458838dd999380e863c9ee92e26d06233
Bob Muscarella <bob.muscarella@gmail.com>
2015-01-26 16:17:20 +0100

    Create get.user.R

---
+++ R/get.user.R
@@ -0,0 +1,8 @@
+#############################################################
+#########	MAKE USER-DEFINED EVALUATION GROUPS	#############
+#############################################################
+
+get.user <- function(occ.grp, bg.grp){
+	out <- list(occ.grp=occ.grp, bg.grp=bg.grp)
+	return(out)	
+}
