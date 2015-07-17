8a37a5e5220bcfaeb55fc650498dbc7419659a6d
Bob Muscarella <bob.muscarella@gmail.com>
2015-07-17 14:59:47 +0200

    Issue warning when NA for env layers at pres/bg points

--- R/tuning.R
+++ R/tuning.R
@@ -26,6 +26,14 @@ tuning <- function (occ, env, bg.coords, occ.grp, bg.grp, method, maxent.args,
   nk <- length(unique(group.data$occ.grp))
   pres <- as.data.frame(extract(env, occ))
   bg <- as.data.frame(extract(env, bg.coords))
+if{ 
+	any(is.na(colSums(pres))
+	message("Warning: some predictors variables are NA at occurrence points")
+	}
+if{ 
+	any(is.na(colSums(bg))
+	message("Warning: some predictors variables are NA at background points")
+	}
   if (!is.null(categoricals)) {
     for (i in 1:length(categoricals)) {
     pres[, categoricals[i]] <- as.factor(pres[, categoricals[i]])
