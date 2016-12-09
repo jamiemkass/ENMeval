54ba59aa12f884641f5456e74921f2c58c9cc04c
Bob Muscarella <bob.muscarella@gmail.com>
2016-12-09 09:55:05 +0100

    Warning if input items are not vectors

--- R/get.user.R
+++ R/get.user.R
@@ -1,6 +1,9 @@
 #############################################################
 #########	MAKE USER-DEFINED EVALUATION GROUPS	#############
 #############################################################
+if(class(occ.grp) == 'data.frame' | class(bg.grp) == 'data.frame'){ 
+	warning('For user-defined partitioning, occ.grp and bg.grp must be supplied aas vectors.')
+}
 
 get.user <- function(occ.grp, bg.grp){
 	out <- list(occ.grp=occ.grp, bg.grp=bg.grp)
