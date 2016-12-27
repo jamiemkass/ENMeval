ac7f849dc5faa45cf4b87c6c6798c0061e10ad26
ndimhypervol <jamie.m.kass@gmail.com>
2016-12-27 01:39:16 -0500

    oops missed a comma
    Former-commit-id: 56157b001f25c30164f2e90fad4dbcd9ca28bd3c

--- R/ENMevaluate.R
+++ R/ENMevaluate.R
@@ -3,7 +3,7 @@ ENMevaluate <- function (occ, env, bg.coords = NULL, occ.grp = NULL, bg.grp = NU
                          categoricals = NULL, n.bg = 10000, method = NULL, overlap = FALSE,
                          aggregation.factor = c(2, 2), kfolds = NA, bin.output = FALSE, clamp = TRUE,
                          rasterPreds = TRUE, parallel = FALSE, numCores = NULL, progbar = TRUE, 
-                         updateProgress = FALSE ...) {
+                         updateProgress = FALSE, ...) {
 
   ptm <- proc.time()
   if (is.null(method)) {
