ee29074d5adcf4ae9b28d4b04bdaa5eb65d1e0d2
Bob Muscarella <bob.muscarella@gmail.com>
2017-01-04 18:06:53 -0700

    var.importance doc and code
    Former-commit-id: 64ad64ebd87c5f12abae3dc83952d57b2fc8da77

---
+++ R/var.importance.R
@@ -0,0 +1,10 @@
+var.importance <- function(mod) {
+  res <- mod@results
+  pc <- res[grepl('contribution', rownames(res)),]
+  pi <- res[grepl('permutation', rownames(res)),]
+  varnames <- sapply(strsplit(names(pc), '.contribution'), function(x) x[1])
+  df <- data.frame(variable=varnames, percent.contribution=pc, permutation.importance=pi, row.names=NULL)
+  df <- df[rev(order(df[,2])),]
+  rownames(df) <- NULL
+  return(df)
+}
