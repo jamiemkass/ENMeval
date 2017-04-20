a4bb77862a3f2f5e5d651bae386735ddd1ba8b89
ndimhypervol <jamie.m.kass@gmail.com>
2017-04-20 11:20:03 -0400

    modifications to ENMevaluate to accept maxnet
    Former-commit-id: 4cb60618b6aa04a7c01eda270a03760b7d8a3575

--- R/ENMevaluate.R
+++ R/ENMevaluate.R
@@ -2,37 +2,46 @@ ENMevaluate <- function (occ, env, bg.coords = NULL, occ.grp = NULL, bg.grp = NU
                          RMvalues = seq(0.5, 4, 0.5), fc = c("L", "LQ", "H", "LQH", "LQHP", "LQHPT"),
                          categoricals = NULL, n.bg = 10000, method = NULL, overlap = FALSE,
                          aggregation.factor = c(2, 2), kfolds = NA, bin.output = FALSE, clamp = TRUE,
-                         rasterPreds = TRUE, parallel = FALSE, numCores = NULL, progbar = TRUE, 
+                         rasterPreds = TRUE, java = FALSE, parallel = FALSE, numCores = NULL, progbar = TRUE, 
                          updateProgress = FALSE, ...) {
 
   ptm <- proc.time()
   if (is.null(method)) {
     stop("Evaluation method needs to be specified.")
   }
-  userArgs <- list(...)
-  allMaxentArgs <- c("addsamplestobackground", "addallsamplestobackground", "allowpartialdata", 
-                     "beta_threshold", "beta_categorical", "beta_lqp", "beta_hinge", "convergencethreshold",
-                     "defaultprevalence", "extrapolate", "fadebyclamping", "jackknife", "maximumbackground", 
-                     "maximumiterations", "removeduplicates")
-  if (length(userArgs) == 0) {
-    userArgs <- NULL
-  } else {
-    if (!all(names(userArgs) %in% allMaxentArgs)) {
-      stop("The maxent argument given is not implemented in ENMeval or is misspelled.")
+  
+  # if maxent.jar is run, specify args command
+  if (java == TRUE) {
+    userArgs <- list(...)
+    allMaxentArgs <- c("addsamplestobackground", "addallsamplestobackground", "allowpartialdata", 
+                       "beta_threshold", "beta_categorical", "beta_lqp", "beta_hinge", "convergencethreshold",
+                       "defaultprevalence", "extrapolate", "fadebyclamping", "jackknife", "maximumbackground", 
+                       "maximumiterations", "removeduplicates")
+    if (length(userArgs) == 0) {
+      userArgs <- NULL
     } else {
-      userArgs <- paste(names(userArgs), unlist(userArgs), sep='=')
+      if (!all(names(userArgs) %in% allMaxentArgs)) {
+        stop("The maxent argument given is not implemented in ENMeval or is misspelled.")
+      } else {
+        userArgs <- paste(names(userArgs), unlist(userArgs), sep='=')
+      }
     }
+    maxent.args <- make.args(RMvalues, fc)
+    args.lab <- make.args(RMvalues, fc, labels = TRUE)
   }
 
+  # if no background points specified, generate random ones
   if (is.null(bg.coords)) {
     bg.coords <- randomPoints(env[[1]], n = n.bg)
   }
-  maxent.args <- make.args(RMvalues, fc)
-  args.lab <- make.args(RMvalues, fc, labels = TRUE)
+  
+  # coerce occ and bg.coords to data frame and rename columns
   occ <- as.data.frame(occ)
   colnames(occ) <- c("LON", "LAT")
   bg.coords <- as.data.frame(bg.coords)
   colnames(bg.coords) <- c("LON", "LAT")
+  
+  # print message for selected cross-validation method
   message(ifelse(method == "jackknife", "Doing evaluations using k-1 jackknife...",
                  ifelse(method == "checkerboard1", "Doing evaluations using checkerboard 1...",
                         ifelse(method == "checkerboard2", "Doing evaluations using checkerboard 2...",
@@ -40,10 +49,14 @@ ENMevaluate <- function (occ, env, bg.coords = NULL, occ.grp = NULL, bg.grp = NU
                                       ifelse(method == "randomkfold", "Doing random k-fold evaluation groups...",
                                              ifelse(method == "user", "Doing user-defined evaluation groups...",
                                                     "Error: You need to specify an accepted evaluation method. Check the documentation.")))))))
+  
+  # run internal tuning function
   results <- tuning(occ, env, bg.coords, occ.grp, bg.grp, method,
                     maxent.args, args.lab, categoricals, aggregation.factor,
-                    kfolds, bin.output, clamp, rasterPreds, parallel, 
+                    kfolds, bin.output, clamp, rasterPreds, java, parallel, 
                     numCores, progbar, updateProgress, userArgs)
+  
+  # if niche overlap selected, calculate and add the resulting matrix to results
   if (overlap == TRUE) {
     if (length(maxent.args) > 1) {
       if(nlayers(results@predictions) > 1) {
@@ -59,9 +72,12 @@ ENMevaluate <- function (occ, env, bg.coords = NULL, occ.grp = NULL, bg.grp = NU
       message("Need >1 settings to do niche overlap")
     }
   }
+  
+  # calculate time expended and print message
   timed <- proc.time() - ptm
   t.min <- floor(timed[3] / 60)
   t.sec <- timed[3] - (t.min * 60)
   message(paste("ENMeval completed in", t.min, "minutes", round(t.sec, 1), "seconds."))
+  
   return(results)
 }
