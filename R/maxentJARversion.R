9fcbf36e33d560b0fb769e072e6708ac792ac41f
ndimhypervol <jamie.m.kass@gmail.com>
2017-04-24 15:20:46 -0400

    new function to get maxent.jar version
    Former-commit-id: 0081f028379ee4c347da3b04cc3c3c4faff8b325

---
+++ R/maxentJARversion.R
@@ -0,0 +1,17 @@
+
+maxentJARversion <- function() {
+  if (is.null(getOption('dismo_rJavaLoaded'))) {
+    # to avoid trouble on macs
+    Sys.setenv(NOAWT=TRUE)
+    if ( requireNamespace('rJava') ) {
+      rJava::.jpackage('dismo')
+      options(dismo_rJavaLoaded=TRUE)
+    } else {
+      stop('rJava cannot be loaded')
+    }
+  }
+  mxe <- rJava::.jnew("meversion") 
+  v <- try(rJava::.jcall(mxe, "S", "meversion"))
+  return(v)
+}
+
