95e9ea84441c22591f00fb685f1b75c23b4515d3
ndimhypervol <jamie.m.kass@gmail.com>
2017-05-10 14:46:02 -0400

    did same for modelTune.maxentJar
    Former-commit-id: df555e50e077dc7221a1ed988433652dbe29017f

--- R/modelTune.maxentJar.R
+++ R/modelTune.maxentJar.R
@@ -2,84 +2,65 @@
 #########	MODEL TUNE for maxent.jar #############
 #################################################
 
-modelTune.maxentJar <- function(pres, bg, env, nk, group.data, args, userArgs, 
-                                rasterPreds, clamp, progbar, updateProgress) {
+modelTune.maxentJar <- function(pres, bg, env, nk, group.data, args.i, userArgs, 
+                                rasterPreds, clamp) {
   
-  if (progbar==TRUE) {
-    pb <- txtProgressBar(0, length(args), style = 3)
+  # set up data: x is coordinates of occs and bg, 
+  # p is vector of 0's and 1's designating occs and bg
+  x <- rbind(pres, bg)
+  p <- c(rep(1, nrow(pres)), rep(0, nrow(bg)))
+  
+  # build the full model from all the data
+  full.mod <- dismo::maxent(x, p, args = c(args.i, userArgs),
+                            factors = categoricals)  
+  
+  # if rasters selected, predict for the full model
+  if (rasterPreds == TRUE) {
+    pred.args <- c("outputformat=raw", ifelse(clamp==TRUE, "doclamp=true", "doclamp=false"))
+    predictive.map <- predict(full.mod, env, args = pred.args)  
+  } else {
+    predictive.map <- stack()
   }
-  out <- list()
   
-  for (i in 1:length(args)) {
-    # set up the console progress bar (progbar), 
-    # or the shiny progress bar (updateProgress)
-    if (length(args) > 1) {
-      if (is.function(updateProgress)) {
-        text <- paste0('Running ', args.lab[[1]][i], args.lab[[2]][i], '...')
-        updateProgress(detail = text)
-      } else if (progbar==TRUE) {
-        setTxtProgressBar(pb, i)
-      }
-    }
+  # set up empty vectors for stats
+  AUC.TEST <- double()
+  AUC.DIFF <- double()
+  OR10 <- double()
+  ORmin <- double()
+  
+  # cross-validation on partitions
+  for (k in 1:nk) {
+    # set up training and testing data groups
+    train.val <- pres[group.data$occ.grp != k, ]
+    test.val <- pres[group.data$occ.grp == k, ]
+    bg.val <- bg[group.data$bg.grp != k, ]
+    # redefine x and p for partition groups
+    x <- rbind(train.val, bg.val)
+    p <- c(rep(1, nrow(train.val)), rep(0, nrow(bg.val)))
     
-    # set up data: x is coordinates of occs and bg, 
-    # p is vector of 0's and 1's designating occs and bg
-    x <- rbind(pres, bg)
-    p <- c(rep(1, nrow(pres)), rep(0, nrow(bg)))
+    # run the current test model
+    mod <- dismo::maxent(x, p, args = c(args.i, userArgs), factors = categoricals)  
     
-    # build the full model from all the data
-    full.mod <- dismo::maxent(x, p, args = c(args[[i]], userArgs),
-                              factors = categoricals)  
+    AUC.TEST[k] <- dismo::evaluate(test.val, bg, mod)@auc
+    AUC.DIFF[k] <- max(0, dismo::evaluate(train.val, bg, mod)@auc - AUC.TEST[k])
     
-    # if rasters selected, predict for the full model
-    if (rasterPreds == TRUE) {
-      pred.args <- c("outputformat=raw", ifelse(clamp==TRUE, "doclamp=true", "doclamp=false"))
-      predictive.map <- predict(full.mod, env, args = pred.args)  
+    # predict values for training and testing data
+    p.train <- predict(mod, train.val, args = pred.args)
+    p.test <- predict(mod, test.val, args = pred.args)  
+    
+    # figure out 90% of total no. of training records
+    if (nrow(train.val) < 10) {
+      n90 <- floor(nrow(train.val) * 0.9)
     } else {
-      predictive.map <- stack()
+      n90 <- ceiling(nrow(train.val) * 0.9)
     }
-    
-    # set up empty vectors for stats
-    AUC.TEST <- double()
-    AUC.DIFF <- double()
-    OR10 <- double()
-    ORmin <- double()
-    
-    # cross-validation on partitions
-    for (k in 1:nk) {
-      # set up training and testing data groups
-      train.val <- pres[group.data$occ.grp != k, ]
-      test.val <- pres[group.data$occ.grp == k, ]
-      bg.val <- bg[group.data$bg.grp != k, ]
-      # redefine x and p for partition groups
-      x <- rbind(train.val, bg.val)
-      p <- c(rep(1, nrow(train.val)), rep(0, nrow(bg.val)))
-      
-      # run the current test model
-      mod <- dismo::maxent(x, p, args = c(args[[i]], userArgs), factors = categoricals)  
-      
-      AUC.TEST[k] <- dismo::evaluate(test.val, bg, mod)@auc
-      AUC.DIFF[k] <- max(0, dismo::evaluate(train.val, bg, mod)@auc - AUC.TEST[k])
-      
-      # predict values for training and testing data
-      p.train <- predict(mod, train.val, args = pred.args)
-      p.test <- predict(mod, test.val, args = pred.args)  
-      
-      # figure out 90% of total no. of training records
-      if (nrow(train.val) < 10) {
-        n90 <- floor(nrow(train.val) * 0.9)
-      } else {
-        n90 <- ceiling(nrow(train.val) * 0.9)
-      }
-      train.thr.10 <- rev(sort(p.train))[n90]
-      OR10[k] <- mean(p.test < train.thr.10)
-      train.thr.min <- min(p.train)
-      ORmin[k] <- mean(p.test < train.thr.min)
-    }
-    stats <- c(AUC.DIFF, AUC.TEST, OR10, ORmin)
-    out[[i]] <- list(full.mod, stats, predictive.map)
+    train.thr.10 <- rev(sort(p.train))[n90]
+    OR10[k] <- mean(p.test < train.thr.10)
+    train.thr.min <- min(p.train)
+    ORmin[k] <- mean(p.test < train.thr.min)
   }
-  if (progbar == TRUE) close(pb)
-  return(out)
+  stats <- c(AUC.DIFF, AUC.TEST, OR10, ORmin)
+  out.i <- list(full.mod, stats, predictive.map)
+  return(out.i)
 }
 
