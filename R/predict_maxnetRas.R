05b6d988087fd16859395f9f9c4cb3832b917378
ndimhypervol <jamie.m.kass@gmail.com>
2017-12-04 11:56:38 -0500

    error catch for na rows in predict func for maxnet

--- R/predict_maxnetRas.R
+++ R/predict_maxnetRas.R
@@ -6,6 +6,13 @@
 predict_maxnetRas <- function(mod, env, type, clamp) {
   env.n <- nlayers(env)
   env.pts <- rasterToPoints(env)
+  origNrow <- nrow(env.pts)
+  env.pts <- na.omit(env.pts)
+  naOmitNrow <- nrow(env.pts)
+  rowDiff <- origNrow - naOmitNrow
+  if (rowDiff > 0) {
+    message(paste('\n', rowDiff, "grid cells found with at least one NA value: these cells were not processed."))
+  }
   mxnet.p <- predict(mod, env.pts, type=type, clamp=clamp)
   env.pts <- cbind(env.pts, as.numeric(mxnet.p))
   mxnet.p <- rasterFromXYZ(env.pts[,c(1, 2, env.n+3)], res=res(env))
