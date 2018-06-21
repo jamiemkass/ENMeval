d73b23a680cd267a4b28b9c4d324110bc0779892
Bob Muscarella <bob.muscarella@gmail.com>
2018-06-21 21:40:24 +0200

    Add error message if tried with maxnet model
    Former-commit-id: 0cad412c0e2f96ce89ddf0507f35ae69d2e9bfe9

--- R/var.importance.R
+++ R/var.importance.R
@@ -1,8 +1,12 @@
 var.importance <- function(mod) {
-  res <- mod@results
-  pc <- res[grepl('contribution', rownames(res)),]
-  pi <- res[grepl('permutation', rownames(res)),]
-  varnames <- sapply(strsplit(names(pc), '.contribution'), function(x) x[1])
-  df <- data.frame(variable=varnames, percent.contribution=pc, permutation.importance=pi, row.names=NULL)
-  return(df)
+  if(!'MaxEnt' %in% class(mod)){
+    stop('Sorry, variable importance cannot currently be calculated with Maxnet models (only Maxent)')
+    } else {
+    res <- mod@results
+    pc <- res[grepl('contribution', rownames(res)),]
+    pi <- res[grepl('permutation', rownames(res)),]
+    varnames <- sapply(strsplit(names(pc), '.contribution'), function(x) x[1])
+    df <- data.frame(variable=varnames, percent.contribution=pc, permutation.importance=pi, row.names=NULL)
+    return(df)
+  }
 }
