c6094f4384825aa49dbe13dec17117f004e31ac0
BobMuscarella <bob.muscarella@gmail.com>
2018-07-23 11:38:18 +0200

    Revert "added version that inlcudes niche overlap output"
    This reverts commit 1c51f74e9932d5c948939b81ae6e98f7459bd77f [formerly 27cec523860c97469ca1d083fcb6b9c10d45eb19].
    
    
    Former-commit-id: 2c133daca06feb0f6628323b8dbe24010ebeaf34

--- R/predict.maxnetRas.R
+++ R/predict.maxnetRas.R
@@ -3,9 +3,9 @@
 #################################################
 
 # function to make a raster prediction from a maxnet object
-maxnet.predictRaster <- function(mod, env, type, clamp) {
-  env.n <- raster::nlayers(env)
-  env.pts <- raster::rasterToPoints(env)
+predict.maxnetRas <- function(mod, env, type, clamp) {
+  env.n <- nlayers(env)
+  env.pts <- rasterToPoints(env)
   origNrow <- nrow(env.pts)
   env.pts <- na.omit(env.pts)
   naOmitNrow <- nrow(env.pts)
@@ -13,9 +13,8 @@ maxnet.predictRaster <- function(mod, env, type, clamp) {
   if (rowDiff > 0) {
     message(paste('\n', rowDiff, "grid cells found with at least one NA value: these cells were excluded from raster predictions."))
   }
-  # mxnet.p <- maxnet::predict(mod, env.pts, type=type, clamp=clamp)
   mxnet.p <- predict(mod, env.pts, type=type, clamp=clamp)
   env.pts <- cbind(env.pts, as.numeric(mxnet.p))
-  mxnet.p <- raster::rasterFromXYZ(env.pts[,c(1, 2, env.n+3)], res=raster::res(env))
+  mxnet.p <- rasterFromXYZ(env.pts[,c(1, 2, env.n+3)], res=res(env))
   return(mxnet.p)
 }
