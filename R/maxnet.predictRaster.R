27cec523860c97469ca1d083fcb6b9c10d45eb19
BobMuscarella <bob.muscarella@gmail.com>
2018-07-23 11:22:59 +0200

    added version that inlcudes niche overlap output

--- R/maxnet.predictRaster.R
+++ R/maxnet.predictRaster.R
@@ -3,9 +3,9 @@
 #################################################
 
 # function to make a raster prediction from a maxnet object
-predict.maxnetRas <- function(mod, env, type, clamp) {
-  env.n <- nlayers(env)
-  env.pts <- rasterToPoints(env)
+maxnet.predictRaster <- function(mod, env, type, clamp) {
+  env.n <- raster::nlayers(env)
+  env.pts <- raster::rasterToPoints(env)
   origNrow <- nrow(env.pts)
   env.pts <- na.omit(env.pts)
   naOmitNrow <- nrow(env.pts)
@@ -13,8 +13,9 @@ predict.maxnetRas <- function(mod, env, type, clamp) {
   if (rowDiff > 0) {
     message(paste('\n', rowDiff, "grid cells found with at least one NA value: these cells were excluded from raster predictions."))
   }
+  # mxnet.p <- maxnet::predict(mod, env.pts, type=type, clamp=clamp)
   mxnet.p <- predict(mod, env.pts, type=type, clamp=clamp)
   env.pts <- cbind(env.pts, as.numeric(mxnet.p))
-  mxnet.p <- rasterFromXYZ(env.pts[,c(1, 2, env.n+3)], res=res(env))
+  mxnet.p <- raster::rasterFromXYZ(env.pts[,c(1, 2, env.n+3)], res=raster::res(env))
   return(mxnet.p)
 }
