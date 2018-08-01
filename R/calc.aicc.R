24cce77ff4167055e1ad86c42135c185af1fd078
Bob Muscarella <bob.muscarella@gmail.com>
2018-08-01 20:40:23 +0200

    Update calc.aicc.R
    Former-commit-id: f77f967e71e2c1d4d71fa8eea232efd388a47056

--- R/calc.aicc.R
+++ R/calc.aicc.R
@@ -5,7 +5,7 @@
 calc.aicc <- function(nparam, occ, predictive.maps) {
   AIC.valid <- nparam < nrow(occ)
   if (nlayers(predictive.maps) == 0) {
-    res <- data.frame(cbind(AICc=NA, delta.AICc=NA, w.AIC=NA, nparam=nparam))
+    res <- data.frame(cbind(AICc=NA, delta.AICc=NA, w.AIC=NA, parameters=nparam))
     warning("Cannot calculate AICc when rasterPreds = FALSE... returning NA's.")
   } else {
     vals <- extract(predictive.maps, occ)
@@ -19,7 +19,7 @@ calc.aicc <- function(nparam, occ, predictive.maps) {
     AICc[is.infinite(AICc)] <- NA
     if(sum(is.na(AICc))==length(AICc)){
       warning("AICc not valid... returning NA's.")
-      res <- data.frame(cbind(AICc, delta.AICc=NA, w.AIC=NA, nparam=nparam))
+      res <- data.frame(cbind(AICc, delta.AICc=NA, w.AIC=NA, parameters=nparam))
     } else {
       delta.AICc <- (AICc - min(AICc, na.rm=TRUE))
       w.AIC <- (exp(-0.5*delta.AICc))/(sum(exp(-0.5*delta.AICc), na.rm=TRUE))
