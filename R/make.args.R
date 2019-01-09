eee7a5e763cf1261b2c0d862a313596dfe78a843
ndimhypervol <jamie.m.kass@gmail.com>
2019-01-09 06:51:12 -0500

    simplify partitioning logic, renaming for clarity

--- R/make.args.R
+++ R/make.args.R
@@ -4,38 +4,38 @@
 
 make.args <- function(mod.settings, algorithm, labels=FALSE) {
 
+  if(algorithm %in% c("maxent.jar", "maxnet")) {
+    rm <- mod.settings[["rm"]]
+    fc <- mod.settings[["fc"]]
+    rm.lab <- rep(rm, each=length(fc))
+    fc.lab <- rep(fc, times=length(rm))
+    args.lab <- list(fc.lab, rm.lab)
+  }
+  
   if(algorithm == "maxent.jar") {
     other.args <- c("noaddsamplestobackground", "noremoveDuplicates", "noautofeature")
     args.list <- list()
-    rms <- mod.settings[["rms"]]
-    fcs <- mod.settings[["fcs"]]
     
-    for(i in 1:length(fcs)) {
+    for(i in 1:length(fc)) {
       args.list[[i]] <- other.args
-      if(!grepl("L", fcs[i])) args.list[[i]] <- c(args.list[[i]], "nolinear")
-      if(!grepl("Q", fcs[i])) args.list[[i]] <- c(args.list[[i]], "noquadratic")
-      if(!grepl("H", fcs[i])) args.list[[i]] <- c(args.list[[i]], "nohinge")
-      if(!grepl("P", fcs[i])) args.list[[i]] <- c(args.list[[i]], "noproduct")
-      if(!grepl("T", fcs[i])) args.list[[i]] <- c(args.list[[i]], "nothreshold")
+      if(!grepl("L", fc[i])) args.list[[i]] <- c(args.list[[i]], "nolinear")
+      if(!grepl("Q", fc[i])) args.list[[i]] <- c(args.list[[i]], "noquadratic")
+      if(!grepl("H", fc[i])) args.list[[i]] <- c(args.list[[i]], "nohinge")
+      if(!grepl("P", fc[i])) args.list[[i]] <- c(args.list[[i]], "noproduct")
+      if(!grepl("T", fc[i])) args.list[[i]] <- c(args.list[[i]], "nothreshold")
     }
     
-    rms.lab <- rep(rms, each=length(fcs))
-    rms.arg <- paste("betamultiplier=", rms.lab, sep="")
-    fcs.lab <- rep(fcs, times=length(rms))
-    fcs.arg <- rep(args.list, times=length(rms))
-
-    args <- list()
-    for(i in 1:length(fcs.arg)) args[[i]] <- c(rms.arg[i], fcs.arg[[i]])
-    args.lab <- list(fcs.lab, rms.lab)
-  }
-  
-  if(algorithm == "maxnet") {
-    rms <- mod.settings[["rms"]]
-    fcs <- mod.settings[["fcs"]]
     
-    fcs.arg <- as.list(tolower(rep(fcs, times=length(rms))))
-    rms.arg <- as.list(sort(rep(rms, times=length(fcs))))
-    args <- mapply(c, fcs.arg, rms.arg, SIMPLIFY=FALSE)
+    rm.arg <- paste("betamultiplier=", rm.lab, sep="")
+    fc.arg <- rep(args.list, times=length(rm))
+    args <- list()
+    for(i in 1:length(fc.arg)) args[[i]] <- c(rm.arg[i], fc.arg[[i]])
+  }
+
+  if(algorithm == "maxnet") {
+    fc.arg <- as.list(tolower(rep(fc, times=length(rm))))
+    rm.arg <- as.list(sort(rep(rm, times=length(fc))))
+    args <- mapply(c, fc.arg, rm.arg, SIMPLIFY=FALSE)
   }
   
   if(labels == FALSE) return(args) else return(args.lab)
