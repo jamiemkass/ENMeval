6887d8278f1964875139c6b482c4b301b4025d12
ndimhypervol <jamie.m.kass@gmail.com>
2019-02-11 16:39:15 -0500

    fixes to var calc, etc
    Former-commit-id: 8cbee2487ea81d2765c80562af9d8a7863c7264b

--- data/ex_analysis.R
+++ data/ex_analysis.R
@@ -8,33 +8,33 @@
 # tune.args <- list(rm = 1:4, fc = c("L", "LQ"))
 # partitions <- "block"
 # categoricals <- "biome"
-
-# SWD
+# 
+# # SWD
 # colnames(bg) <- c("longitude", "latitude")
 # occs.vals <- raster::extract(envs, occs)
 # bg.vals <- raster::extract(envs, bg)
 # 
 # # divide all grid cells in study extent into same partition groups
 # # as the real occurrence data
-# envs.xy <- rasterToPoints(envs[[1]], spatial = TRUE)
-# envs.folds <- ENMeval::get.block(occ=occs, bg.coords=envs.xy@coords)$bg.grp
-#
-
-# old ENMeval
+# # envs.xy <- rasterToPoints(envs[[1]], spatial = TRUE)
+# # envs.folds <- ENMeval::get.block(occ=occs, bg.coords=envs.xy@coords)$bg.grp
+# 
+# 
+# ## old ENMeval
 # e <- ENMevaluate(occs, envs, bg, alg = "maxnet", fc = c("L", "LQ"), RMvalues = 1:4, categoricals = "biome", method = "block")
-
-## regular run
+# 
+# ## regular run
 # e <- ENMevaluate(occs, envs, bg, mod.fun = maxnet::maxnet, tune.args = tune.args, categoricals = "biome", partitions = "block")
-## run with SWD
+# ## run with SWD
 # e <- ENMevaluate(occs, bg = bg, occs.vals = occs.vals, bg.vals = bg.vals, mod.fun = maxnet::maxnet, tune.args = tune.args, categoricals = "biome", partitions = "block")
-## run with independent testing data
+# ## run with independent testing data
 # e <- ENMevaluate(occs[1:250,], envs, bg, mod.fun = maxnet::maxnet, tune.args = tune.args, categoricals = "biome", partitions = "independent", occs.ind = occs[251:nrow(occs),])
-## run with just AIC
+# ## run with just AIC
 # e <- ENMevaluate(occs, envs, bg, mod.fun = maxnet::maxnet, tune.args = tune.args, categoricals = "biome", partitions = "none")
-## run with maxent.jar
+# ## run with maxent.jar
 # e <- ENMevaluate(occs, envs, bg, mod.fun = dismo::maxent, tune.args = tune.args, categoricals = "biome", partitions = "block")
-## run with BRT
+# ## run with BRT
 # tune.args <- list(tree.complexity = 1, learning.rate = 0.1, bag.fraction = 0.5)
 # e <- ENMevaluate(occs, envs, bg, mod.fun = dismo::gbm.step, tune.args = tune.args, categoricals = "biome", partitions = "block")
-# run with BIOCLIM
-# e <- ENMevaluate(occs, envs, bg, mod.fun = dismo::bioclim, categoricals = "biome", partitions = "block")
+# ## run with BIOCLIM
+# e <- ENMevaluate(occs, envs, bg = bg, mod.fun = dismo::bioclim, tune.args = NULL, categoricals = "biome", partitions = "block")
