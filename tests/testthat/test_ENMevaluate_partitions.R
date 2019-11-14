085a8cb7cf22557c82b77e979c5042d58ee8e616
Jamie Kass <jamie.m.kass@gmail.com>
2019-11-14 17:05:35 +0900

    fixed error with factors and ind eval
    Former-commit-id: bf5a783d7a9283978d6dad536d202c4f8a5fb5e7

--- tests/testthat/test_ENMevaluate_partitions.R
+++ tests/testthat/test_ENMevaluate_partitions.R
@@ -7,33 +7,39 @@ envs <- raster::stack(list.files(path=paste(system.file(package='dismo'), '/ex',
                                  pattern='grd', full.names=TRUE))
 bg <- as.data.frame(dismo::randomPoints(envs, 1000))
 names(bg) <- names(occs)
-tune.args <- list(fc = c("L","LQ","H"), rm = 1:5)
+# tune.args <- list(fc = c("L","LQ","H"), rm = 1:5)
 tune.args <- list(fc = "L", rm = 2:3)
 # random sample of occs for runs that use subsets
 i <- sample(1:nrow(occs))
 
+e <- list()
 # maxnet run with tuning parameters, categorical variable, block partitions, and niche overlap
-e.block <- ENMevaluate(occs, envs, bg, mod.name = "maxnet", tune.args = tune.args, categoricals = "biome", 
+e$b <- ENMevaluate(occs, envs, bg, mod.name = "maxnet", tune.args = tune.args, categoricals = "biome", 
                   partitions = "block", overlap = TRUE)
 # checkerboard1 partitions
-e.cb1 <- ENMevaluate(occs, envs, bg, mod.name = "maxnet", tune.args = tune.args, categoricals = "biome", 
+e$cb1 <- ENMevaluate(occs, envs, bg, mod.name = "maxnet", tune.args = tune.args, categoricals = "biome", 
                   partitions = "checkerboard1", overlap = TRUE)
 # checkerboard2 partitions
-e.cb2 <- ENMevaluate(occs, envs, bg, mod.name = "maxnet", tune.args = tune.args, categoricals = "biome", 
+e$cb2 <- ENMevaluate(occs, envs, bg, mod.name = "maxnet", tune.args = tune.args, categoricals = "biome", 
                   partitions = "checkerboard2", overlap = TRUE)
 # random k-fold partitions
-e <- ENMevaluate(occs, envs, bg, mod.name = "maxnet", tune.args = tune.args, categoricals = "biome", 
+e$r <- ENMevaluate(occs, envs, bg, mod.name = "maxnet", tune.args = tune.args, categoricals = "biome", 
                   partitions = "randomkfold", kfolds = 4, overlap = TRUE)
 # jackknife partitions
-e5 <- ENMevaluate(occs[i[1:10],], envs, bg, mod.name = "maxnet", tune.args = tune.args, categoricals = "biome", 
+e$j <- ENMevaluate(occs[i[1:10],], envs, bg, mod.name = "maxnet", tune.args = tune.args, categoricals = "biome", 
                   partitions = "jackknife", overlap = TRUE)
 # independent partition
-e6 <- ENMevaluate(occs[i[1:450],], envs, bg, mod.name = "maxnet", tune.args = tune.args, categoricals = "biome", 
+e$i <- ENMevaluate(occs[i[1:450],], envs, bg, mod.name = "maxnet", tune.args = tune.args, categoricals = "biome", 
                   partitions = "independent", occs.ind = occs[i[451:nrow(occs)],], overlap = TRUE)
 # no partitions
-e7 <- ENMevaluate(occs, envs, bg, mod.name = "maxnet", tune.args = tune.args, categoricals = "biome", 
+e$n <- ENMevaluate(occs, envs, bg, mod.name = "maxnet", tune.args = tune.args, categoricals = "biome", 
                   partitions = "none", overlap = TRUE)
 
 
-# test_that("")
+test_that("" {
+  for(x in 1:length(e)) {
+    
+  }
+  
+})
 
